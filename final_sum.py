22e17d876b2067e6733cf4db1e6f9bc4e652b9cb
Anon <68425389+Snowerest@users.noreply.github.com>
2021-03-18 17:07:54 +0800

    Update final_sum.py

--- final_sum.py
+++ final_sum.py
@@ -3,3 +3,6 @@ def final_sum(num: int) -> int:
         return num
     return final_sum(sum([int(i) for i in str(num)]))
         
+
+def final_sum2(num: int) -> int:
+    return num if num < 10 else final_sum(sum([int(i) for i in str(num)]))
