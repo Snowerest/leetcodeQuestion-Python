e2265270b757dc861f872dd0ebb5374e6fadd777
Anon <68425389+Snowerest@users.noreply.github.com>
2021-03-18 17:13:25 +0800

    Update final_multipy.py

--- final_multipy.py
+++ final_multipy.py
@@ -5,3 +5,7 @@ def final_multipy(num: int) -> int:
     for i in str(num):
         n *= int(i)
     return final_multipy(n)
+
+
+def final_multipy2(num: int) -> int:
+    return num if num < 10 else final_multipy2(reduce(lambda x, y: x*y, [int(i) for i in str(num)]))
